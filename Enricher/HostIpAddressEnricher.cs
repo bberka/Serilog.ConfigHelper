2d04009c3cfd8265ec34ed143235d4f6c43a6ffc
berkaybyr <berkaybyr@icloud.com>
2023-06-29 01:52:32 +0300

    bug fix
    Signed-off-by: berkaybyr <berkaybyr@icloud.com>

--- Enricher/HostIpAddressEnricher.cs
+++ Enricher/HostIpAddressEnricher.cs
@@ -22,7 +22,9 @@ public class HostIpAddressEnricher : ILogEventEnricher
 
     private static string GetHostIpAddress() {
         var ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-        var ipAddress = ipHostInfo.AddressList[0];
-        return ipAddress.ToString();
+                var ipAddress = ipHostInfo.AddressList.Select(x => x.ToString())
+                                          .Where(x => !x.StartsWith("192.168.") && !x.StartsWith("::"))
+                                          .ToList();
+                return ipAddress.FirstOrDefault();
     }
 }
